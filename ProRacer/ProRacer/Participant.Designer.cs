75281caf69bc771b69b9f5a15f3fa04f58069659
kimmy <kimmy.zuo@pictureworks.biz>
2018-01-30 11:34:16 -0400

    / ‘ProRacer/ProRacer/Participant.Designer.cs’

--- ProRacer/ProRacer/Participant.Designer.cs
+++ ProRacer/ProRacer/Participant.Designer.cs
@@ -96,6 +96,7 @@
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "&Add";
             this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += System.EventHandler();
             // 
             // btnUpdate
             // 
