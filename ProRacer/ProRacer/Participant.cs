1b2a35394f6314763bb629a3d3950ef8bae469c8
kimmy <kimmy.zuo@pictureworks.biz>
2018-01-30 11:08:51 -0400

    / ‘ProRacer/ProRacer/Participant.cs’

--- ProRacer/ProRacer/Participant.cs
+++ ProRacer/ProRacer/Participant.cs
@@ -25,8 +25,8 @@ namespace ProRacer
         private void Participant_Load(object sender, EventArgs e)
         {
             this.conn = new SqlConnection("server=sqlserver.cv4bnwlhigjt.ca-central-1.rds.amazonaws.com,1433;Database=ProRacer;Network Library=DBMSSOCN;integrated security=false;User ID=singemazuo;Password=z28397562");
-            this.cmmd = new SqlCommand("SELECT * FROM Participant");
-            this.daAuthen = new SqlDataAdapter();
+            this.cmmd = new SqlCommand("SELECT * FROM Participant",this.conn);
+            this.daAuthen = new SqlDataAdapter(this.cmmd);
             this.cmmd.CommandText = "SELECT * FROM Participant";
 
             lstSearch.DataBindings.Add("DataSource",this.daAuthen,"*");
