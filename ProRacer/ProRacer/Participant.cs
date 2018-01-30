cda2499c19210b97969856e7a3d78467bc9c0e28
kimmy <kimmy.zuo@pictureworks.biz>
2018-01-30 11:38:37 -0400

    / ‘ProRacer/ProRacer/Participant.cs’

--- ProRacer/ProRacer/Participant.cs
+++ ProRacer/ProRacer/Participant.cs
@@ -24,7 +24,7 @@ namespace ProRacer
 
         private void Participant_Load(object sender, EventArgs e)
         {
-            this.conn = new SqlConnection("server=sqlserver.cv4bnwlhigjt.ca-central-1.rds.amazonaws.com,1433;Database=ProRacer;Network Library=DBMSSOCN;integrated security=false;User ID=singemazuo;Password=z28397562");
+            SqlConnection conn = new SqlConnection("server=sqlserver.cv4bnwlhigjt.ca-central-1.rds.amazonaws.com,1433;Database=ProRacer;Network Library=DBMSSOCN;integrated security=false;User ID=singemazuo;Password=z28397562");
             this.cmmd = new SqlCommand("SELECT * FROM Participant WHERE Country = "+txtCountry.Text,this.conn);
             this.daAuthen = new SqlDataAdapter(this.cmmd);
             DataSet ds = new DataSet();
