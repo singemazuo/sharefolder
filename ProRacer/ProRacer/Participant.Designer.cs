ac2edb584560ac774ce5a595dd92bafec94e71cf
kimmy <kimmy.zuo@pictureworks.biz>
2018-01-30 11:34:53 -0400

    / ‘ProRacer/ProRacer/Participant.Designer.cs’

--- ProRacer/ProRacer/Participant.Designer.cs
+++ ProRacer/ProRacer/Participant.Designer.cs
@@ -128,7 +128,7 @@
             this.btnCancel.TabIndex = 7;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
-            this.btnUpdate.Click += System.EventHandler(BtnUpdate_Click);
+
 
             // 
             // btnDelete
