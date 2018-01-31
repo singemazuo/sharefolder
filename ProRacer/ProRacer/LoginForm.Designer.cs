22933b559221ec96449746fa78883b6acfb979b8
kimmy <kimmy.zuo@pictureworks.biz>
2018-01-31 12:04:47 -0400

    + ‘ProRacer/’ + ‘ProRacer/.vs/ProRacer/v15/.suo’ + ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ + ‘ProRacer/ProRacer.sln’ + ‘ProRacer/ProRacer.userprefs’ + ‘ProRacer/ProRacer/App.config’ + ‘ProRacer/ProRacer/LoginForm.Designer.cs’ + ‘ProRacer/ProRacer/LoginForm.cs’ + ‘ProRacer/ProRacer/LoginForm.resx’ + ‘ProRacer/ProRacer/MainForm.Designer.cs’ ...

---
+++ ProRacer/ProRacer/LoginForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ProRacer
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtUserName = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.CbxPassword = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(356, 241);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(375, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "&User Name:";
+            // 
+            // TxtUserName
+            // 
+            this.TxtUserName.Location = new System.Drawing.Point(378, 45);
+            this.TxtUserName.Name = "TxtUserName";
+            this.TxtUserName.Size = new System.Drawing.Size(210, 20);
+            this.TxtUserName.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(378, 116);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(210, 20);
+            this.TxtPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(375, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "&Password:";
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Location = new System.Drawing.Point(378, 187);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(89, 23);
+            this.BtnOk.TabIndex = 5;
+            this.BtnOk.Text = "&Ok";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(496, 187);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(92, 23);
+            this.BtnCancel.TabIndex = 6;
+            this.BtnCancel.Text = "&Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // CbxPassword
+            // 
+            this.CbxPassword.AutoSize = true;
+            this.CbxPassword.Location = new System.Drawing.Point(378, 153);
+            this.CbxPassword.Name = "CbxPassword";
+            this.CbxPassword.Size = new System.Drawing.Size(121, 17);
+            this.CbxPassword.TabIndex = 7;
+            this.CbxPassword.Text = "Remember paaword";
+            this.CbxPassword.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 244);
+            this.Controls.Add(this.CbxPassword);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "LoginForm";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtUserName;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.CheckBox CbxPassword;
+    }
+}
---
+++ ProRacer/ProRacer/PRDatabaseManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProRacer
+{
+    public interface IPRDataCommands
+    {
+        
+    }
+
+    public class PRDatabaseManager
+    {
+        private static PRDatabaseManager _instance;
+        private SqlConnection conn;
+
+        private PRDatabaseManager(string url)
+        {
+            this.conn = new SqlConnection(url);
+            SqlCommand cmmd = new SqlCommand("SELECT * FROM Participant", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Country");
+        }
+
+        public static PRDatabaseManager Create(string url = "Data Source=sqlserver.cv4bnwlhigjt.ca-central-1.rds.amazonaws.com,1433;Initial Catalog=ProRacer;User ID=singemazuo;Password=z28397562")
+        {
+            if(_instance == null)
+            {
+                _instance = new PRDatabaseManager(url);
+            }
+            else
+            {
+                if(!_instance.conn.ConnectionString.Equals(url))
+                {
+                    _instance = new PRDatabaseManager(url);
+                }
+            }
+            return _instance;
+        }
+    }
+}
