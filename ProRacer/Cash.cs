833807c25590028ac11e0437097e48ea00968f99
5028212 <singemazuo@gmail.com>
2018-02-07 07:26:46 -0800

    + ‘ProRacer/Cash.Designer.cs’ + ‘ProRacer/Cash.cs’ + ‘ProRacer/Cash.resx’

---
+++ ProRacer/Cash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Cash : Form
+    {
+        public Cash()
+        {
+            InitializeComponent();
+        }
+
+       
+}
