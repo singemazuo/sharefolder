abba30aae78621a6b217bae6e27eb6ecda9efe75
5028212 <singemazuo@gmail.com>
2018-02-07 08:59:30 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/Cash.cs’

--- ProRacer/ProRacer/Cash.cs
+++ ProRacer/ProRacer/Cash.cs
@@ -18,7 +18,7 @@ namespace ProRacer
             lstRace.DataSource = ds;
             lstRace.DisplayMember = "Race.Name";
             lstRace.ValueMember = "Race.RaceId";
-            lstRace.DataBindings.Add("SelectedValue",);
+            lstRace.DataBindings.Add("SelectedValue", ds, "Result.");
         }
 
         private void lstRace_SelectedIndexChanged(object sender, EventArgs e)
