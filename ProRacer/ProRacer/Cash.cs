dbedfdad85c1ae5bba4c00dbcc21726d75f6d69f
5028212 <singemazuo@gmail.com>
2018-02-07 09:36:11 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/Cash.cs’

--- ProRacer/ProRacer/Cash.cs
+++ ProRacer/ProRacer/Cash.cs
@@ -20,7 +20,7 @@ namespace ProRacer
             lstRace.DataSource = dsSpo;
             lstRace.DisplayMember = "Sponsor.SponsorName";
             lstRace.ValueMember = "Sponsor.SponsorId";
-            lstRace.DataBindings.Add("SelectedValue", dsPar, "Participant.SponsorId");
+            lstParticipants.DataBindings.Add("SelectedValue", dsPar, "Participant.SponsorId");
 
             lstParticipants.DataSource = dsPar;
             lstParticipants.DisplayMember = "Participant.FullName";
