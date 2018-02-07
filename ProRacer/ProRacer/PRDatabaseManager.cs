08c2941b186c8d7d5b0acff921a50ad5a099dc40
5028212 <singemazuo@gmail.com>
2018-02-07 09:12:11 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/PRDatabaseManager.cs’

--- ProRacer/ProRacer/PRDatabaseManager.cs
+++ ProRacer/ProRacer/PRDatabaseManager.cs
@@ -21,7 +21,7 @@ namespace ProRacer
             {
                 if (_participant == null)
                 {
-                    SqlCommand cmmd = new SqlCommand("SELECT * FROM Participant", conn);
+                    SqlCommand cmmd = new SqlCommand("SELECT LastName, FirstName, FirstName + ',' + LastName AS [FullName] FROM Participant", conn);
                     SqlDataAdapter dapater = new SqlDataAdapter(cmmd);
                     _participant = new DataSet();
                     dapater.Fill(_participant, "Participant");
