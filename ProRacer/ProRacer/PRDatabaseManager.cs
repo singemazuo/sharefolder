3e74d5a5a2231b02495eb1fbdd765c313778afe1
5028212 <singemazuo@gmail.com>
2018-02-07 07:48:11 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/PRDatabaseManager.cs’

--- ProRacer/ProRacer/PRDatabaseManager.cs
+++ ProRacer/ProRacer/PRDatabaseManager.cs
@@ -38,6 +38,11 @@ namespace ProRacer
             dapater = new SqlDataAdapter(cmmd);
             race = new DataSet();
             dapater.Fill(race);
+
+            cmmd = new SqlCommand("SELECT * FROM Race", conn);
+            dapater = new SqlDataAdapter(cmmd);
+            race = new DataSet();
+            dapater.Fill(race);
         }
 
         public static PRDatabaseManager instance(string url = "Data Source=sqlserver.cv4bnwlhigjt.ca-central-1.rds.amazonaws.com,1433;Initial Catalog=ProRacer;User ID=singemazuo;Password=z28397562")
