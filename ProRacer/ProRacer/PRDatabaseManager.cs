1eca04c18293ba8b50bc3bb17a1a46ba6f49b57f
5028212 <singemazuo@gmail.com>
2018-02-07 12:03:43 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/PRDatabaseManager.cs’

--- ProRacer/ProRacer/PRDatabaseManager.cs
+++ ProRacer/ProRacer/PRDatabaseManager.cs
@@ -94,7 +94,7 @@ namespace ProRacer
 
         public DataSet SponsorWithParticipant(string name = null)
         {
-            SqlCommand cmmd = new SqlCommand("SELECT SponsorID,SponsorName,LastName + ',' + FirstName AS FullName FROM Sponsor", conn);
+            SqlCommand cmmd = new SqlCommand("SELECT SponsorID,SponsorName,LastName + ',' + FirstName AS FullName FROM Sponsor INNER JOIN Participant", conn);
             SqlDataAdapter adapter = new SqlDataAdapter(cmmd);
             DataSet ds = new DataSet();
             if (name != null)
