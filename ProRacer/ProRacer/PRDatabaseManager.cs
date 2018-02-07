c2cf11164b6eab72d02bc766464c38ff251b733d
5028212 <singemazuo@gmail.com>
2018-02-07 07:35:43 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘roRacer/ProRacer/PRDatabaseManager.cs’

--- ProRacer/ProRacer/PRDatabaseManager.cs
+++ ProRacer/ProRacer/PRDatabaseManager.cs
@@ -16,11 +16,11 @@ namespace ProRacer
 
         private PRDatabaseManager(string url)
         {
-            this.conn = new SqlConnection(url);
-            SqlCommand cmmd = new SqlCommand("SELECT * FROM Participant", conn);
-            SqlDataAdapter da = new SqlDataAdapter(cmmd);
-            DataSet ds = new DataSet();
-            da.Fill(ds, "Country");
+            //this.conn = new SqlConnection(url);
+            //SqlCommand cmmd = new SqlCommand("SELECT * FROM Participant", conn);
+            //SqlDataAdapter da = new SqlDataAdapter(cmmd);
+            //DataSet ds = new DataSet();
+            //da.Fill(ds, "Country");
 
             SqlConnection conn = new SqlConnection("Data Source=sqlserver.cv4bnwlhigjt.ca-central-1.rds.amazonaws.com,1433;Initial Catalog=ProRacer;User ID=singemazuo;Password=z28397562");
             SqlCommand cmmd = new SqlCommand("SELECT * FROM Participant", conn);
