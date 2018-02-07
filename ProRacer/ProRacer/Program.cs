7fb5aa22a1039a778b2b9631625318e435bd7cd9
5028212 <singemazuo@gmail.com>
2018-02-07 07:28:18 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/Program.cs’

--- ProRacer/ProRacer/Program.cs
+++ ProRacer/ProRacer/Program.cs
@@ -14,6 +14,7 @@ namespace ProRacer
         [STAThread]
         static void Main()
         {
+            Cash cash = new Cash();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
