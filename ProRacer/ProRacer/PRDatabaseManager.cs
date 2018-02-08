9f01ca5e776220e4c1db3c145444dfb89966fd75
5028212 <singemazuo@gmail.com>
2018-02-07 16:11:58 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/PRDatabaseManager.cs’

--- ProRacer/ProRacer/PRDatabaseManager.cs
+++ ProRacer/ProRacer/PRDatabaseManager.cs
@@ -85,7 +85,10 @@ namespace ProRacer
                     adapter = new SqlDataAdapter(cmmd);
                     adapter.Fill(_ds, "Sponsor");
 
-
+                    cmmd = new SqlCommand("SELECT * FROM Race", conn);
+                    SqlDataAdapter dapater = new SqlDataAdapter(cmmd);
+                    _race = new DataSet();
+                    dapater.Fill(_race, "Race");
                 }
                 return _ds;
             }
