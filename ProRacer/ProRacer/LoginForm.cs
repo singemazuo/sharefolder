b72a1f0da8c41c037fe0175f429806174bfcdc88
5028212 <singemazuo@gmail.com>
2018-02-08 06:41:50 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/LoginForm.cs’

--- ProRacer/ProRacer/LoginForm.cs
+++ ProRacer/ProRacer/LoginForm.cs
@@ -17,7 +17,7 @@ namespace ProRacer
         private SqlCommand cmmd;
         private MainForm mainForm;
 
-        public LoginForm()
+        public LoginForm(MainForm form)
         {
             this.mainForm = form;
 
