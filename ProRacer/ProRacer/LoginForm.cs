c02d006fa67fa5a7a2eb775bea8cb1ea7ed2b408
5028212 <singemazuo@gmail.com>
2018-02-08 06:52:50 -0800

    / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/LoginForm.cs’

--- ProRacer/ProRacer/LoginForm.cs
+++ ProRacer/ProRacer/LoginForm.cs
@@ -52,6 +52,7 @@ namespace ProRacer
                 }
                 else
                 {
+                    MessageBox.Show();
                     Close();
                 }
             }
