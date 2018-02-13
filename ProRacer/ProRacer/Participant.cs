adc705de19d92b6aa7a39a8f6ee253247431f35b
5028212 <singemazuo@gmail.com>
2018-02-13 08:35:22 -0800

    / ‘ProRacer/.vs/ProRacer/v15/.suo’ / ‘ProRacer/.vs/ProRacer/v15/sqlite3/storage.ide’ / ‘ProRacer/ProRacer/Participant.cs’ / ‘ProRacer/ProRacer/bin/Debug/ProRacer.exe’ / ‘ProRacer/ProRacer/bin/Debug/ProRacer.pdb’ / ‘ProRacer/ProRacer/obj/Debug/ProRacer.exe’ / ‘ProRacer/ProRacer/obj/Debug/ProRacer.pdb’

--- ProRacer/ProRacer/Participant.cs
+++ ProRacer/ProRacer/Participant.cs
@@ -30,7 +30,7 @@ namespace ProRacer
             cmbParticipants.DataSource = ds;
             cmbParticipants.DisplayMember = "Participant.FullName";
             cmbParticipants.ValueMember = "Participant.ParticipantId";
-            cmbParticipants.DataBindings.Add("SelectedValue", ds, "Participant.ParticipantId");
+            cmbParticipants.DataBindings.Add("SelectedValue", ds, "Sponsor.ParticipantId");
 
             txtFirstName.DataBindings.Add("Text",ds, "Participant.FirstName");
 
